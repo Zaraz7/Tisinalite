f5f17533ed7a63b37f646e20d031d34375b1e3d4
Maxim Terlyck <zaraz121801@gmail.com>
2022-05-01 10:07:15 +0300

    Добавление Avalonia в проект

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Tisinalite.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
