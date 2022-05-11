31ebe40144c4f48b03a074ae77b7968c9956cb78
Maxim Terlik <zaraz121801@gmail.com>
2022-05-11 15:02:32 +0300

    Удаление неиспользуемых зависимостей

--- Tisinalite/Pages/Input.xaml.cs
+++ Tisinalite/Pages/Input.xaml.cs
@@ -1,22 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Tisinalite.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Input.xaml
-    /// </summary>
     public partial class Input : Window
     {
         public string Entry;
